a8c8e0f6a19c92067603d28b22dcc6eed8b53079
Sauss <46446673+Ylanz123@users.noreply.github.com>
2023-07-31 16:49:44 +1000

    Worked on how threading will occur.

--- MainLogic/MainLogic.py
+++ MainLogic/MainLogic.py
@@ -5,11 +5,24 @@ from MathFunctions.MathFunctions import *
 
 from threading import Thread
 
+# List for storing all threads. This is done in global as we can wait for a thread
+# or delete them at any time during execution
+# Store threads like this: <name, thread>
+#   .append(["movement", Thread(target=robot.do_something)])
+# Current thread types:
+#   "movement", "scanning", "txrx"
+global_thread_list = {}
+
 def run_main_logic():
     # Creat class instances
     arena_map = Map()
     start_pose = Pose(1, 3, pi / 2)
     robot = Robot(start_pose)
+
+    # Initialise extra threads
+    global_thread_list["movement"] = None
+    global_thread_list["scanning"] = None
+    global_thread_list["txrx"] = None
 
     # Enter logic loop
     logic_loop(robot=robot, arena_map=arena_map)
@@ -31,8 +44,8 @@ def state_handler(robot=None, arena_map=None):
     if robot.get_state() == "scanning":
         handle_scanning(robot=robot)
 
-    if robot.get_state() == "delivering":
-        handle_delivering(robot=robot, arena_map=arena_map)
+    if robot.get_state() == "delivering" or robot.get_state() == "returning":
+        handle_delivering_and_returning(robot=robot, arena_map=arena_map)
 
 def handle_waiting(robot=None):
     # TODO
@@ -56,16 +69,23 @@ def handle_scanning(robot=None):
 
     if robot.get_sub_state() == "scanning":
         # Do a scan attempt
-        # If scan is successful, set the sub-state to "success"
-        robot.set_state(sub_state="success")
+        scan_result = robot.scan_attempt()
 
-def handle_delivering(robot=None, arena_map=None):
+        # If scan is successful, set the sub-state to "success"
+        if scan_result:
+            robot.set_state(sub_state="success")
+
+    if robot.get_sub_state() == "success":
+        # Set state to make robot begin delivering the package
+        robot.set_state(state="delivering")
+
+def handle_delivering_and_returning(robot=None, arena_map=None):
     if robot.get_sub_state() is None:
         robot.set_state(sub_state="planning")
 
     if robot.get_sub_state() == "planning":
         # Plan a path with the given obstacles and other parameters
-        robot.plan_path(arena_map=arena_map, end_pose=robot.packages[0].destination.deposit_pose)
+        robot.plan_path(arena_map=arena_map, end_pose=robot.get_current_goal(arena_map=arena_map))
 
         # Check if there is a valid path planned
         if robot.path is not None:
@@ -73,26 +93,52 @@ def handle_delivering(robot=None, arena_map=None):
             robot.set_state(sub_state="moving")
 
     if robot.get_sub_state() == "moving":
-        # Tick moving flag
-        robot.is_moving = True
+        # Check if robot isn't moving
+        if not robot.is_moving and not robot.movement_complete:
+            # Tick moving flag
+            robot.is_moving = True
 
-        # Start thread to control movement
-        moving_thread = Thread(target=robot.follow_path)
+            # Start thread to control movement
+            global_thread_list["movement"] = Thread(target=robot.follow_path)
+            global_thread_list["movement"].start()
+        elif not robot.is_moving and robot.movement_complete:
+            # Reset movement complete flag
+            robot.movement_complete = False
 
-def state_selector(current_state=None, current_sub_state=None):
-    """
-    Determines how the current state affects the next state
-    :param current_state: The current state of the robot
-    :param current_substate: The current sub-state of the robot if there is one
-    :return: The next state of the robot
-    """
-    # If state is delivering
-    if current_state == "delivering":
-        # and substate is stuck, replan the route
-        if current_substate == "stuck":
-            return "planning", None
+            # Robot successfully arrived at position
+            robot.set_state(sub_state="positioned")
 
-    # If state is finished, the robot task is complete
+    if robot.get_sub_state() == "stuck":
+        # Make a beep sound
+        robot.sound(sound="beep")
+
+        # Set robot sub-state to planning once again
+        robot.set_state(sub_state="planning")
+
+    if robot.get_sub_state() == "positioned":
+        # Set sub-state to depositing
+        robot.set_state(sub_state="depositing")
+
+    if robot.get_state() == "delivering" and robot.get_sub_state() == "depositing":
+        if not robot.depositing:
+            robot.depositing = True
+            # Start the package depositing process
+            robot.deposit_package()
+
+        if not robot.depositing:
+            robot.set_state(sub_state="completed")
+
+    if robot.get_sub_state() == "completed":
+        if not robot.scanning_flag:
+            # Tick continuous scan flag
+            robot.scanning_flag = True
+
+            # Start thread to begin scanning for packages
+            global_thread_list["scanning"] = Thread(target=robot.continuous_scan)
+            global_thread_list["scanning"].start()
+
+        # Return to pick-up location
+        robot.set_state(state="returning")
 
 def wait_for_start_button():
     # If start button is pressed, break otherwise wait.
--- Robot/Robot.py
+++ Robot/Robot.py
@@ -20,6 +20,7 @@ class Robot:
         # Sub-states of "scanning":
         #   "scanning": A scanning attempt is being made
         #   "success": Scanning succeeded and tag has been decoded
+        self.scanning_flag = False
         # Sub-states of "delivering" and "returning":
         #   "planning": Robot is planning its route through an arena. This will happen at the beginning and anytime an obstacle interferes with the current route
         #   "moving": Robot is moving along its planned route
@@ -31,6 +32,33 @@ class Robot:
         #   "depositing": The robot is depositing the package into the destination
         self.path = None  # Path class that holds all the information about the robots current path
         self.packages = None  # List of packages and their destinations in order of how they are placed. index 0 is the first package
+        self.depositing = False
+
+    def get_current_goal(self, arena_map=None):
+        if self.packages is not None:
+            return self.packages[0].destination.deposit_pose
+        else:
+            return arena_map.pickup_location
+
+    def continuous_scan(self):
+        # As long as state is returning, attempt scanning
+        while self.state == "returning" and self.scanning_flag:
+            # Do scan attempt
+            scan_result = self.scan_attempt()
+
+            # If scan result is False scan again
+            if not scan_result:
+                continue
+
+            # If scan is successful,
+
+    def scan_attempt(self):
+        """
+        Makes a scan attempt using the RF scanner, if a package is detected return True, otherwise return False
+        """
+        # Do one attempt to scan the RF tag of a potential package
+        # TODO
+        pass
 
     def follow_path(self):
         # As long as the is_moving flag is True
@@ -41,7 +69,23 @@ class Robot:
         # When the is_moving flag is False for any reason, stop the robot's movement
         # TODO
 
+    def deposit_package(self):
+        # Deposit the next package
+        # TODO
+        pass
+
+    def sound(self, sound=None):
+        if sound == "beep":
+            # Play beep sound
+            # TODO
+            pass
+
     def plan_path(self, arena_map=None):
+        # TODO
+        pass
+
+    def handle_txrx(self):
+        # Transmits and receives data to and from other robots
         # TODO
         pass
 
