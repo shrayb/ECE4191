06f6e480e0ca83f4cabf7569c72d6ae97e478ffd
Nic <njdellaportas@hotmail.com>
2023-08-02 12:31:44 +1000

    Added simple high low test

---
+++ TestingComponents/High_Low Test.py
@@ -0,0 +1,19 @@
+import FakeRPi.GPIO as GPIO
+from time import sleep
+
+test_pin = 3
+
+def setup():
+    GPIO.setup(test_pin, GPIO.OUT)
+
+def loop():
+    while True:
+        # Turn high
+        GPIO.output(test_pin, GPIO.HIGH)
+
+        sleep(1)
+
+        # Turn low
+        GPIO.output(test_pin, GPIO.LOW)
+
+        sleep(1)
