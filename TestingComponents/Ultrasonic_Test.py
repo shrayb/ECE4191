6ab4f0cb7a8dbe4fa68a86cd1066140f92949f49
Nic <njdellaportas@hotmail.com>
2023-08-09 12:09:20 +1000

    Added ultrasonic test

--- TestingComponents/Ultrasonic_Test.py
+++ TestingComponents/Ultrasonic_Test.py
@@ -10,29 +10,30 @@ def setup():
     GPIO.setup(echo_pin, GPIO.IN)
 
 def main_loop():
-    # Trigger the ultrasonic sensor
-    GPIO.output(trigger_pin, GPIO.HIGH)
+    while True:
+        # Trigger the ultrasonic sensor
+        GPIO.output(trigger_pin, GPIO.HIGH)
 
-    # Wait 0.00001
-    sleep(0.00001)
+        # Wait 0.00001
+        sleep(0.00001)
 
-    # Stop the trigger
-    GPIO.output(trigger_pin, GPIO.LOW)
-    start_time = time()
-    stop_time = time()
-
-    # Save start time
-    while GPIO.input(echo_pin) == 0:
+        # Stop the trigger
+        GPIO.output(trigger_pin, GPIO.LOW)
         start_time = time()
-
-    # Save time echo arrives
-    while GPIO.input(echo_pin) == 1:
         stop_time = time()
 
-    time_elapsed = stop_time - start_time
-    distance = (time_elapsed * 343000) / 2
+        # Save start time
+        while GPIO.input(echo_pin) == 0:
+            start_time = time()
 
-    print("Distance:", distance, "mm")
+        # Save time echo arrives
+        while GPIO.input(echo_pin) == 1:
+            stop_time = time()
+
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 343000) / 2
+
+        print("Distance:", distance, "mm")
 
 
 if __name__ == "__main__":
