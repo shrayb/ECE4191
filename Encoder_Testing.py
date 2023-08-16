91e22c027b36465504b162b32364e495a9438745
Nic <njdellaportas@hotmail.com>
2023-08-16 11:02:29 +1000

    Added support for both wheels

--- Encoder_Testing.py
+++ Encoder_Testing.py
@@ -1,3 +1,5 @@
+import math
+
 import RPi.GPIO as GPIO
 from time import time, sleep
 
@@ -11,26 +13,41 @@ motor_left_enable = 18
 motor_left_encoder_a = 17
 motor_left_encoder_b = 27
 
+motor_right_positive = 22
+motor_right_negative = 23
+motor_right_enable = 24
+motor_right_encoder_a = 10
+motor_right_encoder_b = 9
+
 GPIO.setmode(GPIO.BCM)
 
 left_motor = Motor(motor_left_enable, motor_left_positive, motor_left_negative, motor_left_encoder_a, motor_left_encoder_b)
+right_motor = Motor(motor_right_enable, motor_right_positive, motor_right_negative, motor_right_encoder_a, motor_right_encoder_b)
 
 def encoder_loop():
     while True:
         left_motor.update_encoder()
+        right_motor.update_encoder()
 
 def loop():
     # Turn wheels forward at 50 percent speed
     left_motor.set_speed(50)
+    right_motor.set_speed(50)
     left_motor.forward()
+    right_motor.forward()
 
     encoder_thread = Thread(target=encoder_loop)
     encoder_thread.start()
 
     while True:
-        distance = 0.0035 * left_motor.ticks
-        print("Ticks:", left_motor.ticks)
-        print("Distance:", distance, "m")
+        circum = math.pi * 55
+        cpr = 48 * 75
+        tick_dist = circum / cpr
+        left_distance = tick_dist * left_motor.ticks
+        right_distance = tick_dist * right_motor.ticks
+        # print("Ticks:", left_motor.ticks)
+        print("Left distance:", left_distance, "mm")
+        print("Right distance:", right_distance, "mm")
         sleep(0.25)
 
 
