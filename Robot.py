3a4afe1a7a70f605640993ae9f719ee887bd3d72
Nic <njdellaportas@hotmail.com>
2023-08-30 20:42:37 +1000

    Detection

--- Robot.py
+++ Robot.py
@@ -145,7 +145,7 @@ class Robot:
         while True:
             flag, coords_x, coords_y, th = self.detect_obstacle(self.front_left_ultrasonic, self.front_right_ultrasonic)
             if flag:
-                print("Obstacle Found at:", coords_x / 1000, coords_y / 1000)
+                print("Obstacle Found at:", coords_x, coords_y)
                 # Add the new found obstacle
                 self.map_class.add_obstacle_to_grid(th, Pose(coords_x / 1000, coords_y / 1000))
 
