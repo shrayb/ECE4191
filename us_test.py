f041085bd455368c4e67a2e474df47640a3bf237
UMAR_ABDULLAH_AL_ABEDIN <uabe0001@student.monash.edu>
2023-08-30 18:32:59 +1000

    asd

--- us_test.py
+++ us_test.py
@@ -3,7 +3,15 @@ import numpy as np
 import RPi.GPIO as GPIO
 from time import time, sleep
 from threading import Thread
-from BaseClasses import Motor, Pose, Ultrasonic
+from BaseClasses import Ultrasonic
 from Robot import Robot
 
 
+
+def loop():
+
+
+
+if __name__ == "__main__":
+    while (True):
+        loop()
