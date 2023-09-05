415a760ff4d9b21ba72919c78609663715905ad9
Nic <njdellaportas@hotmail.com>
2023-09-05 20:38:02 +1000

    Old code to work

--- Milestone_1.py
+++ Milestone_1.py
@@ -34,7 +34,7 @@ right_motor = Motor(motor_right_enable, motor_right_positive, motor_right_negati
 front_left_sonic = Ultrasonic(echo_pin=front_left_sonic_echo, trig_pin=front_left_sonic_trig, x_offset=0.155, y_offset=0.01)
 front_right_sonic = Ultrasonic(echo_pin=front_right_sonic_echo, trig_pin=front_right_sonic_trig, x_offset=0.155, y_offset=-0.010)
 
-pose = Pose(0.9, 0.8, 0)
+pose = Pose(0.9, 0.8, math.pi)
 robot = Robot(pose)
 robot.left_motor = left_motor
 robot.right_motor = right_motor
