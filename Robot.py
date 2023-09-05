55a6a53f3de68238f1babce5a2339692f859593f
Nic <njdellaportas@hotmail.com>
2023-09-05 21:23:48 +1000

    Old code to work

--- Robot.py
+++ Robot.py
@@ -127,6 +127,7 @@ class Robot:
             sleep(0.25)
             flag, coords_x, coords_y, th = self.detect_obstacle(self.front_left_ultrasonic, self.front_right_ultrasonic)
             if flag and len(self.path_queue) > 0 and self.path_is_tested:
+                self.is_impending_collision = True
                 # Add the new-found obstacle
                 self.map_class.add_obstacle_to_grid(th, Pose(coords_x, coords_y))
 
