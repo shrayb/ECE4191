711a58c52b33c4e03d46bbcddd151535d8ba392b
Nic <njdellaportas@hotmail.com>
2023-09-05 21:17:17 +1000

    Old code to work

--- Milestone_1.py
+++ Milestone_1.py
@@ -53,8 +53,8 @@ drive_thread.start()
 def loop():
     # Define waypoints to go to in order
     waypoints = [Pose(0.3, 0.2, 0),
-                 Pose(0.9, 0.8)
-                 # Pose(0.3, 0.8),
+                 Pose(0.9, 0.8),
+                 Pose(0.3, 0.8)
                  # Pose(0.3, 0.2, 0)
                 ]
 
