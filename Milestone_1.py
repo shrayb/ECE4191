c6f9a7531acfac5c1664b7fb268e20444955029d
Nic <njdellaportas@hotmail.com>
2023-09-05 19:11:44 +1000

    Old code to work

--- Milestone_1.py
+++ Milestone_1.py
@@ -8,14 +8,14 @@ from threading import Thread
 from BaseClasses import Motor, Pose, Pose, Ultrasonic
 from Robot import Robot
 
-motor_right_positive = 15
-motor_right_negative = 18
+motor_right_positive = 18
+motor_right_negative = 15
 motor_right_enable = 14
 motor_right_encoder_a = 3
 motor_right_encoder_b = 4
 
-motor_left_positive = 17
-motor_left_negative = 27
+motor_left_positive = 27
+motor_left_negative = 17
 motor_left_enable = 22
 motor_left_encoder_a = 23
 motor_left_encoder_b = 24
