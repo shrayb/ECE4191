dd4b9ac7430127807baf0a19a8ddebbbf099779b
Nic <njdellaportas@hotmail.com>
2023-09-05 22:08:54 +1000

    Old code to work

--- Robot.py
+++ Robot.py
@@ -117,6 +117,7 @@ class Robot:
                 print("Popping waypoint")
                 self.path_queue.pop(0)
                 self.path_is_tested = False
+                self.successful_waypoint = False
                 if len(self.path_queue) == 0 and not self.is_impending_collision:
                     print("deleting current goal")
                     self.current_goal = None
