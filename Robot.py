dac060b33e27bcbb7c9c0e644c92655939740b45
Nic <njdellaportas@hotmail.com>
2023-09-05 21:59:12 +1000

    Old code to work

--- Robot.py
+++ Robot.py
@@ -116,7 +116,7 @@ class Robot:
             if self.successful_waypoint:
                 self.path_queue.pop(0)
                 self.path_is_tested = False
-                if len(self.path_queue) == 0:
+                if len(self.path_queue) == 0 and not self.is_impending_collision:
                     print("deleting current goal")
                     self.current_goal = None
 
