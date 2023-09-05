dd1a3a9512b5e6a18851c39270d1a7d426c2e837
Nic <njdellaportas@hotmail.com>
2023-09-05 15:24:11 +1000

    Old code to work

--- Drive_And_Detect.py
+++ Drive_And_Detect.py
@@ -52,17 +52,17 @@ drive_thread.start()
 
 def loop():
     # Define waypoints to go to in order
-    waypoints = [Pose(1.0, 0.0)]
+    robot.path_queue.append([Pose(1.0, 0.0)])
 
     # Loop and travel to each waypoint
     while True:
         if not robot.is_moving:
-            if len(waypoints) > 0:
+            if len(robot.path_queue) > 0:
                 # Travel to next waypoint
                 robot.is_moving = True
-                robot.current_goal = waypoints[0]
+                robot.current_goal = robot.path_queue[0]
                 robot.create_path()
-                waypoints.pop(0)
+                robot.path_queue.pop(0)
 
                 # robot.map_class.plot_grid()
 
