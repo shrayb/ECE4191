60194407080ac168e3e8a379ae5306f87ebdd5df
Nic <njdellaportas@hotmail.com>
2023-09-13 12:54:40 +1000

    Removed map stuff and added simple collision avoidance

--- Hello.py
+++ Hello.py
@@ -54,9 +54,14 @@ drive_thread.start()
 def loop():
     # Define waypoints to go to in order
     waypoints = [[],
-                 Pose(0.8, 0.7),
-                 Pose(0.3, 0.7, math.pi),
-                 Pose(0.3, 0.2, 0)
+                 Pose(0.8, 0.2),
+                 Pose(0.8, 0.8),
+                 Pose(0.3, 0.8),
+                 Pose(0.3, 0.2),
+                 Pose(0.8, 0.2),
+                 Pose(0.8, 0.8),
+                 Pose(0.3, 0.8),
+                 Pose(0.3, 0.2)
                 ]
 
     robot.left_motor.stop()
