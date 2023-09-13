a56db5e3f4a3da5cf2210f6a459384500c54fd27
Nic <njdellaportas@hotmail.com>
2023-09-13 11:53:48 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -62,6 +62,7 @@ class Robot:
         self.safe_reversing = False
         self.ramp_up_percent = 0.2
         self.ramp_down_percent = 0.65
+        self.turn_accuracy_count = 0
 
     def get_current_goal(self):
         if self.package is not None:
@@ -143,6 +144,12 @@ class Robot:
             print("Angle error:", waypoint_error_angle * 180 / math.pi, "degrees")
             if waypoint_error_distance < 0.03 and waypoint_error_angle < (5 * math.pi / 180) and not self.is_impending_collision:  # 3 cm accuracy and 5 degree accuracy
                 self.current_goal = None
+                self.turn_accuracy_count = 0
+            if waypoint_error_distance < 0.03 and waypoint_error_angle >= (5 * math.pi / 180) and self.turn_accuracy_count < 5:
+                self.turn_accuracy_count += 1
+            else:
+                self.current_goal = None
+                self.turn_accuracy_count = 0
 
     def encoder_update_loop(self):
         while True:
@@ -270,7 +277,6 @@ class Robot:
         # Continuously check if the turn has less than 15 degrees of the turn remaining
         if abs(angle) < (15 * math.pi / 180):
             self.tick_check_and_speed_control(turn_ticks, self.slow_speed, is_turning)
-            print("USE SLOW SPEED")
         else:
             self.tick_check_and_speed_control(turn_ticks, self.max_speed, is_turning)
 
