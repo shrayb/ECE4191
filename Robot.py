0c3a5faab30486e047ef676c806e03dc3fbcb2e6
Nic <njdellaportas@hotmail.com>
2023-09-13 10:29:35 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -49,7 +49,7 @@ class Robot:
         self.rear_right_ultrasonic = None  # Rear right ultrasonic sensor class
         self.colour_sensor = None  # ColourSensor class for the colour sensor
         self.turn_radius = 0.13  # Metres
-        self.wheel_radius = 0.055171309 # Metres
+        self.wheel_radius = 0.053761959 # Metres
         self.distance_per_tick = (self.wheel_radius * 2 * math.pi) / (74.83 * 48)  # Distance per tick in metres
         self.max_speed = 50  # Upper percentage for maximum speed
         self.slow_speed = 30  # Upper percentage for slower speed
