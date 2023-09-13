78f9b10cd44a62c097f1e1fb13349b2dc4f2b88a
Nic <njdellaportas@hotmail.com>
2023-09-13 11:12:57 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -51,8 +51,8 @@ class Robot:
         self.turn_radius = 0.131367155  # Metres
         self.wheel_radius = 0.053761959  # Metres
         self.distance_per_tick = (self.wheel_radius * 2 * math.pi) / (74.83 * 48)  # Distance per tick in metres
-        self.max_speed = 40  # Upper percentage for maximum speed
-        self.slow_speed = 35  # Upper percentage for slower speed
+        self.max_speed = 35  # Upper percentage for maximum speed
+        self.slow_speed = 30  # Upper percentage for slower speed
         self.PID_gain = 1.3  # Raise to make the PID more sensitive, lower to make the PID less sensitive
         self.map_size = (2, 2)
         self.sensor_readings = set_default_sensor_readings()  # 5 Sensors by 6 columns
