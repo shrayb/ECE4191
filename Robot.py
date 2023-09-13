bde545615b3abcb1547d036f737e63872cfde182
Nic <njdellaportas@hotmail.com>
2023-09-13 12:49:14 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -144,10 +144,10 @@ class Robot:
             waypoint_error_angle = calculate_angle_difference(angle1=self.pose.theta, angle2=self.current_goal.theta)
             print("Drive error:", waypoint_error_distance * 100, "cm")
             print("Angle error:", waypoint_error_angle * 180 / math.pi, "degrees")
-            if waypoint_error_distance < 0.03 and waypoint_error_angle < (2 * math.pi / 180) and not self.is_impending_collision:  # 3 cm accuracy and 5 degree accuracy
+            if waypoint_error_distance < 0.03 and waypoint_error_angle < (1 * math.pi / 180) and not self.is_impending_collision:  # 3 cm accuracy and 5 degree accuracy
                 self.current_goal = None
                 self.max_tick_factor = 1
-            elif waypoint_error_distance < 0.03 and waypoint_error_angle >= (2 * math.pi / 180) and not self.is_impending_collision:
+            elif waypoint_error_distance < 0.03 and waypoint_error_angle >= (1 * math.pi / 180) and not self.is_impending_collision:
                 self.max_tick_factor *= 0.9
             if self.max_tick_factor < 0.3:
                 self.current_goal = None
