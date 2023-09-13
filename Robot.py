c171ed974561801b4cbc281c0c772aa646e89300
Nic <njdellaportas@hotmail.com>
2023-09-13 11:35:11 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -221,11 +221,11 @@ class Robot:
 
             # At the start ramp up speed slowly, then near the end slow it down slowly. Increases final pose accuracy
             if tick_percentage < self.ramp_up_percent:
-                left_motor_speed *= max(min(tick_percentage / self.ramp_up_percent, self.max_speed), self.slow_speed / 100)
-                right_motor_speed *= max(min(tick_percentage / self.ramp_up_percent, self.max_speed), self.slow_speed / 100)
+                left_motor_speed *= max(min(tick_percentage / self.ramp_up_percent, max_speed), self.slow_speed / 100)
+                right_motor_speed *= max(min(tick_percentage / self.ramp_up_percent, max_speed), self.slow_speed / 100)
             elif tick_percentage > self.ramp_down_percent:
-                left_motor_speed *= max(min((1 - tick_percentage) / (1 - self.ramp_down_percent), self.max_speed), self.slow_speed / 100)
-                right_motor_speed *= max(min((1 - tick_percentage) / (1 - self.ramp_down_percent), self.max_speed), self.slow_speed / 100)
+                left_motor_speed *= max(min((1 - tick_percentage) / (1 - self.ramp_down_percent), max_speed), self.slow_speed / 100)
+                right_motor_speed *= max(min((1 - tick_percentage) / (1 - self.ramp_down_percent), max_speed), self.slow_speed / 100)
 
             self.left_motor.set_speed(left_motor_speed)
             self.right_motor.set_speed(right_motor_speed)
