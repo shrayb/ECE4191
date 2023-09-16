2a44949fd0d746b37e4f41fb1b1753920277f35e
Nic <njdellaportas@hotmail.com>
2023-09-16 13:56:42 +1000

    Removed map stuff and added simple collision avoidance

--- Hello.py
+++ Hello.py
@@ -58,7 +58,6 @@ def loop():
     # Define waypoints to go to in order
     waypoints = [[],
                  Pose(0.65, 0.4, -math.pi/2),
-                 Pose(0.8, 0.8, 3 * math.pi /4),
                  Pose(0.6, 0.3, math.pi/2)
                 ]
 
