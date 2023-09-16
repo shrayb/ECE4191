077315a27923ac2cd85c8c0c921a5de3244915cd
Nic <njdellaportas@hotmail.com>
2023-09-16 14:33:42 +1000

    Removed map stuff and added simple collision avoidance

--- Robot.py
+++ Robot.py
@@ -51,7 +51,7 @@ class Robot:
         self.colour_sensor = None  # ColourSensor class for the colour sensor
         self.turn_radius = 0.12925  # Metres
         self.wheel_radius = 0.05408  # Metres
-        self.distance_per_tick = 9.76998e-05  # Distance per tick in metres
+        self.distance_per_tick = 0.0001276998  # Distance per tick in metres
         self.max_speed = 45  # Upper percentage for maximum speed
         self.slow_speed = 45  # Upper percentage for slower speed
         self.PID_gain = 0.8  # Raise to make the PID more sensitive, lower to make the PID less sensitive
