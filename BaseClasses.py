562be0a1750f466b2501cb7e678061420eab546b
Sauss <46446673+Ylanz123@users.noreply.github.com>
2023-09-23 19:17:44 +1000

    Optimised code

--- BaseClasses.py
+++ BaseClasses.py
@@ -208,14 +208,14 @@ class Package:
 
     def identify_destinations(self):
         if self.colour == "red":
-            self.destination_pose = Pose(x=0.25, y=0.6, theta=math.pi/2)
-            self.return_destination = Pose(x=0.25, y=0.2)
+            self.destination_pose = Pose(x=0.3, y=0.4, theta=math.pi/2)
+            self.return_destination = Pose(x=0.3, y=0.2)
         if self.colour == "green":
-            self.destination_pose = Pose(x=0.75, y=0.6, theta=math.pi/2)
-            self.return_destination = Pose(x=0.75, y=0.2)
+            self.destination_pose = Pose(x=0.3, y=0.6, theta=math.pi/2)
+            self.return_destination = Pose(x=0.3, y=0.2)
         if self.colour == "blue":
-            self.destination_pose = Pose(x=1.25, y=0.6, theta=math.pi/2)
-            self.return_destination = Pose(x=1.25, y=0.2)
+            self.destination_pose = Pose(x=0.3, y=0.8, theta=math.pi/2)
+            self.return_destination = Pose(x=0.3, y=0.2)
 
 class Motor:
     def __init__(self, enable_pin=None, input_a=None, input_b=None, encoder_a=None, encoder_b=None, speed=100):
