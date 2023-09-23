82882c4482fa9513149dd654d780a40c23dfc53c
Nic <njdellaportas@hotmail.com>
2023-09-23 12:13:47 +1000

    Removed map stuff and added simple collision avoidance

--- ColourTest3.py
+++ ColourTest3.py
@@ -2,23 +2,19 @@ import RPi.GPIO as GPIO
 from time import time, sleep
 from BaseClasses import *
 
-s0 = 20
-s1 = 16
-s2 = 7
-s3 = 8
-signal = 1
+s2 = 16
+s3 = 20
+signal = 12
 
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(s0, GPIO.OUT)
-    GPIO.setup(s1, GPIO.OUT)
     GPIO.setup(s2, GPIO.OUT)
     GPIO.setup(s3, GPIO.OUT)
     print("\n")
 
 def loop():
-    colour_sens = ColourSensor(s0, s1, s2, s3, signal)
+    colour_sens = ColourSensor(s0=None, s1=None, s2=s2, s3=s3, signal=signal)
     while True:
         reading = colour_sens.read_colour()
         print("Colour:", reading)
