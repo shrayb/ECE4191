ee7f9f5753f9ed86cc0e700baa875485212219ab
Sauss <46446673+Ylanz123@users.noreply.github.com>
2023-09-23 19:26:24 +1000

    Optimised code

--- Robot.py
+++ Robot.py
@@ -26,7 +26,7 @@ class Robot:
         self.turn_radius = 0.12355  # Metres make bigger to go more make smaller to go less
         self.wheel_radius = 0.05408  # Metres
         self.distance_per_tick = 0.00012265  # Distance per tick in metres make bigger to go less make smaller to go more
-        self.max_speed = 75  # Upper percentage for maximum speed
+        self.max_speed = 50  # Upper percentage for maximum speed
         self.slow_speed = 50  # Upper percentage for slower speed
         self.PID_gain = 4  # Raise to make the PID more sensitive, lower to make the PID less sensitive
         self.PID_turning = 1
