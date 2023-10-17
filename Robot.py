5749ce970e18b2c88cfafe8b9b5cb5de263bb3f7
Nic <njdellaportas@hotmail.com>
2023-10-17 18:46:15 +1100

    Updated test functions

--- Robot.py
+++ Robot.py
@@ -82,7 +82,6 @@ class Robot:
             if self.do_localise:
                 # Localise the robot
                 self.re_localise()
-                self.send_pose_and_goal()
                 self.do_localise = False
 
             # Check if there is an impending collision
